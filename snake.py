775141b82c2a50e7fca0af620d37a63d7f86c12c
David Cervantes <davidcervantesdev@gmail.com>
2024-01-08 17:52:42 -0800

    feat: added directions

--- snake.py
+++ snake.py
@@ -24,6 +24,15 @@ class cube(object):
         j = self.pos[1]
         
         pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
+        if eyes:
+            center = dis // 2
+            radius = 3
+            circleMiddle = (i * dis + center - radius, j * dis + 8)
+            circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
+
+            
 
 class snake(object):
     body = []
